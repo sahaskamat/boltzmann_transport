959f40988cb1986255e686208eae25a8a491bf4d
sahaskamat <sahaskamat@yahoo.co.in>
2023-06-15 14:03:14 -0400

    changed magnetic field axis of rotation WRT initialpoints

--- LSCOorbitschecker.py
+++ LSCOorbitschecker.py
@@ -5,10 +5,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dispersionInstance = dispersion.LSCOdispersion()
-initialpointsInstance = orbitcreation.InitialPoints(5,dispersionInstance,False)
+initialpointsInstance = orbitcreation.InitialPoints(20,dispersionInstance,True)
 
 orbitsinstance = orbitcreation.Orbits(dispersionInstance,initialpointsInstance)
-orbitsinstance.createOrbits([1*np.sin(np.deg2rad(70)),0,1*np.sin(np.deg2rad(70))],0.1)
+orbitsinstance.createOrbits([0,1*np.sin(np.deg2rad(80)),1*np.cos(np.deg2rad(80))],0.1)
 orbitsinstance.createOrbitsEQS(0.05)
 
 conductivityInstance = conductivity.Conductivity(dispersionInstance,orbitsinstance,initialpointsInstance)
@@ -19,6 +19,3 @@ conductivityInstance.createSigma()
 print(initialpointsInstance.k0)
 orbitsinstance.plotOrbitsEQS()
 rhoxy = np.linalg.inv(conductivityInstance.sigma)[0,1]
-
-print(rhoxy)
-print((42E-3)/(rhoxy))
