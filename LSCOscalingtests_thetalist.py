6743e8db2ba02e8c8360fb0798ffcda0a404f987
sahaskamat <sahaskamat@yahoo.co.in>
2023-06-20 15:50:18 -0400

    added timing to LSCO thetalist

--- LSCOscalingtests_thetalist.py
+++ LSCOscalingtests_thetalist.py
@@ -4,6 +4,9 @@ import dispersion
 import orbitcreation
 import conductivity
 from makesigmalist import makelist_parallel
+from time import time
+
+startime = time()
 
 thetalist = np.linspace(0,80,20)
 
@@ -12,8 +15,8 @@ initialpointsInstance = orbitcreation.InitialPoints(40,dispersionInstance,True)
 
 def getsigma(theta):
     orbitsinstance = orbitcreation.Orbits(dispersionInstance,initialpointsInstance)
-    orbitsinstance.createOrbits([0,45*np.sin(np.deg2rad(theta)),45*np.cos(np.deg2rad(theta))],0.05)
-    orbitsinstance.createOrbitsEQS(0.051)
+    orbitsinstance.createOrbits([0,45*np.sin(np.deg2rad(theta)),45*np.cos(np.deg2rad(theta))],0.1)
+    orbitsinstance.createOrbitsEQS(0.101)
     print(f'orbitcreation completed for {theta} degrees')
     #orbitsinstance.plotOrbitsEQS() #enable plotting for diagnostic purposes
     conductivityInstance = conductivity.Conductivity(dispersionInstance,orbitsinstance,initialpointsInstance)
@@ -26,6 +29,9 @@ def getsigma(theta):
 sigmalist,rholist,arealist = makelist_parallel(getsigma,thetalist)
 
 rhoxylist= [rho[2,2]*1E-4 for rho in rholist]
+
+endtime = time()
+print(f"execution time: {endtime-startime}")
 
 plt.scatter(thetalist,rhoxylist)
 plt.ylabel(r"$\rho$ ($10^{-9} m\Omega$ cm )")
