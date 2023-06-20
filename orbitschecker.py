acadb5d3005fe32bfffa04ba09fd9be9683d6fed
sahaskamat <sahaskamat@yahoo.co.in>
2023-06-20 10:11:30 -0400

    added print(n) to orbitschecker

--- orbitschecker.py
+++ orbitschecker.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 dispersionInstance = dispersion.FreeElectronDispersion(1,2,7)
-initialpointsInstance = orbitcreation.InitialPoints(10,dispersionInstance,False)
+initialpointsInstance = orbitcreation.InitialPoints(20,dispersionInstance,False)
 
 
 orbitsinstance = orbitcreation.Orbits(dispersionInstance,initialpointsInstance)
@@ -18,6 +18,7 @@ conductivityInstance.createAlpha()
 conductivityInstance.createSigma()
 rhoxy = np.linalg.inv(conductivityInstance.sigma)[0,1]
 rhoxx = np.linalg.inv(conductivityInstance.sigma)[0,0]
+print(conductivityInstance.n)
 
 print(rhoxy)
 print((42.689824241E-3)/(rhoxy))
