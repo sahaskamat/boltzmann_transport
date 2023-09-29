8d04b2bcb14098eeeb38cbc3f08bd10697ec9c2b
sahaskamat <sahaskamat@yahoo.co.in>
2023-09-29 16:46:48 -0400

    converted free electron tests to rho_xx (revert this commit for rho_xy)

--- FREEscalingtests_thetalist.py
+++ FREEscalingtests_thetalist.py
@@ -23,7 +23,7 @@ print(f"Time taken to create initialcurves = {endtime - starttime}")
 starttime = time()
 def getsigma(theta):
     #Bz stays constant and field angle changes
-    B = [(1*np.sin(np.deg2rad(theta))*np.cos(phi_rad))/(1*np.cos(np.deg2rad(theta))),(1*np.sin(np.deg2rad(theta))*np.sin(phi_rad))/(1*np.cos(np.deg2rad(theta))),1]
+    B = [(0.01*np.sin(np.deg2rad(theta))*np.cos(phi_rad))/(1*np.cos(np.deg2rad(theta))),(0.01*np.sin(np.deg2rad(theta))*np.sin(phi_rad))/(1*np.cos(np.deg2rad(theta))),0.01]
     orbitsinstance = orbitcreation.NewOrbits(dispersionInstance,initialpointsInstance)
     orbitsinstance.createOrbits(B,termination_resolution=0.05,mult_factor=40,sampletimes= np.linspace(0,1,100000),rtol=1e-11,atol=1e-12)
     orbitsinstance.createOrbitsEQS(integration_resolution=0.05)
@@ -44,7 +44,7 @@ sigmalist,rholist,arealist = makelist_parallel(getsigma,thetalist)
 endtime = time()
 print("Time taken to create orbits:",endtime-starttime)
 
-rhoxylist= [rho[0,1]*10e-5 for rho in rholist]
+rhoxylist= [rho[0,0]*10e-5 for rho in rholist]
 
 endtime_global = time()
 print(f"execution time: {endtime_global-starttime_global}")
