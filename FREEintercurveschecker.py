f2583afece3c530c7671ccd2a059a2157d2e47d5
sahaskamat <sahaskamat@yahoo.co.in>
2023-09-29 15:05:33 -0400

    created free electron test files

---
+++ FREEintercurveschecker.py
@@ -0,0 +1,84 @@
+#computation libraries
+import numpy as np
+import matplotlib.pyplot as plt
+
+#benchmarking libraries
+from time import time
+import cProfile, io
+import pstats
+
+#homemade libraries
+import dispersion
+import orbitcreation
+import conductivity
+
+def main():
+
+    dispersionInstance = dispersion.FREEdispersion()
+    initialpointsInstance = orbitcreation.InterpolatedCurves(200,dispersionInstance,True)
+
+    starttime = time()
+    initialpointsInstance.solveforpoints(parallelised=False)
+    initialpointsInstance.extendedZoneMultiply(2)
+    initialpointsInstance.createPlaneAnchors(10)
+    #initialpointsInstance.plotpoints()
+    endtime = time()
+
+    print(f"Time taken to create initialcurves = {endtime - starttime}")
+
+
+    #ax = plt.figure().add_subplot(projection='3d')
+
+    #plotting extendedcurveslist
+    #for curve in initialpointsInstance.extendedcurvesList:
+    #    ax.scatter(curve[:,0],curve[:,1], curve[:,2], label='parametric curve',s=1)
+
+    theta = np.deg2rad(80)
+    phi = np.deg2rad(0)
+    B = [45*np.sin(theta)*np.cos(phi),45*np.sin(theta)*np.sin(phi),45*np.cos(theta)]
+
+    #plottingintersections
+    #ax.scatter(intersections[:,0],intersections[:,1],intersections[:,2],c='#FF0000',s=10)
+
+    starttime = time()
+    orbitsinstance = orbitcreation.NewOrbits(dispersionInstance,initialpointsInstance)
+    orbitsinstance.createOrbits(B,termination_resolution=0.1,mult_factor=15,sampletimes= np.linspace(0,0.5,10000))
+    orbitsinstance.createOrbitsEQS(integration_resolution=0.1)
+
+    plt.show()    
+    orbitsinstance.orbitdiagnosticplot()
+    """
+    listoforbits = orbitsinstance.orbitsEQS
+    endtime = time()
+    print(f"Time taken to create orbits = {endtime - starttime}, number of orbits created {len(orbitsinstance.orbitsEQS)}, time spent finding intitialpoints {orbitsinstance.timespentfindingpoints}")
+
+    starttime = time()
+    conductivityInstance = conductivity.Conductivity(dispersionInstance,orbitsinstance,initialpointsInstance)
+    endtime = time()
+    print(f"Time taken to create conductivityInstance =  {endtime - starttime}")
+
+    starttime = time()
+    conductivityInstance.createAMatrix()
+    print(conductivityInstance.A.shape)
+    endtime = time()
+    print(f"Time taken to create Amatrix =  {endtime - starttime}")
+
+    starttime = time()
+    conductivityInstance.createAlpha()
+    endtime = time()
+    print(f"Time taken to create Alpha =  {endtime - starttime}")
+
+    starttime = time()
+    conductivityInstance.createSigma()
+    endtime = time()
+    print(f"Time taken to calculate conductivity = {endtime - starttime}")
+
+    orbitsinstance.orbitdiagnosticplot()
+
+    #for orbit in listoforbits: ax.scatter(orbit[:,0],orbit[:,1],orbit[:,2],s=1)
+    #plt.show()    
+"""
+
+cProfile.run('main()',filename='stats.prof')
+#main()
+#plt.show()
---
+++ FREEscalingtests_thetalist.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import dispersion
+import orbitcreation
+import conductivity
+from makesigmalist import makelist_parallel
+from time import time
+
+starttime_global = time()
+thetalist = np.linspace(0,75,20)
+phi = 0
+
+phi_rad = np.deg2rad(phi)
+dispersionInstance = dispersion.FREEdispersion()
+initialpointsInstance = orbitcreation.InterpolatedCurves(200,dispersionInstance,False)
+starttime = time()
+initialpointsInstance.solveforpoints(parallelised=False)
+initialpointsInstance.extendedZoneMultiply(5)
+initialpointsInstance.createPlaneAnchors(200)
+endtime = time()
+print(f"Time taken to create initialcurves = {endtime - starttime}")
+
+starttime = time()
+def getsigma(theta):
+    #Bz stays constant and field angle changes
+    B = [(1*np.sin(np.deg2rad(theta))*np.cos(phi_rad))/(1*np.cos(np.deg2rad(theta))),(1*np.sin(np.deg2rad(theta))*np.sin(phi_rad))/(1*np.cos(np.deg2rad(theta))),1]
+    orbitsinstance = orbitcreation.NewOrbits(dispersionInstance,initialpointsInstance)
+    orbitsinstance.createOrbits(B,termination_resolution=0.1,mult_factor=40,sampletimes= np.linspace(0,4,100000),rtol=1e-10,atol=1e-11)
+    orbitsinstance.createOrbitsEQS(integration_resolution=0.1)
+    endtime = time()
+
+    conductivityInstance = conductivity.Conductivity(dispersionInstance,orbitsinstance,initialpointsInstance)
+    conductivityInstance.createAMatrix()
+    conductivityInstance.createAlpha()
+    conductivityInstance.createSigma()
+
+    #if theta<65 and theta>55: orbitsinstance.orbitdiagnosticplot()
+    #print(f'matrixinversion performed for {theta}')
+    print(f"Theta={theta},Calculated total area: {conductivityInstance.areasum}, number of orbits used {len(conductivityInstance.orbitsInstance.orbitsEQS)}")
+    return conductivityInstance.sigma,conductivityInstance.areasum
+
+
+sigmalist,rholist,arealist = makelist_parallel(getsigma,thetalist)
+endtime = time()
+print("Time taken to create orbits:",endtime-starttime)
+
+rhoxylist= [rho[0,1]*10e-5 for rho in rholist]
+
+endtime_global = time()
+print(f"execution time: {endtime_global-starttime_global}")
+
+plt.scatter(thetalist,rhoxylist)
+plt.ylabel(r"$\rho_{xy}$ ($m\Omega$ cm )")
+plt.xlabel(r'$\theta$')
+plt.show()
+
+plt.scatter(thetalist,arealist)
+plt.ylabel(r"$Area (\AA)$ cm )")
+plt.xlabel(r'$\theta$')
+plt.show()
