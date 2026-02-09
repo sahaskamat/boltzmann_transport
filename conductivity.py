a9ca284e358ef0068bfe130a442188ba4ca6954e
sahaskamat <sahaskamat@yahoo.co.in>
2026-02-09 13:55:50 -0500

    separated Amatrix creation into field dependent and field independent parts

--- conductivity.py
+++ conductivity.py
@@ -12,72 +12,83 @@ class Conductivity:
 
     Contains methods to calculate the Amatrix, alpha, and sigma
     """
-    def __init__(self,dispersionInstance,orbitsInstace,initialPointsInstance,B):
+    def __init__(self,dispersionInstance,FSorbitsInstance):
         self.dispersionInstance = dispersionInstance
-        self.orbitsInstance = orbitsInstace
-        self.initialPointsInstance = initialPointsInstance
-        self.B = B
+        self.FSorbitsInstance = FSorbitsInstance
 
-    def createAMatrix(self):
+    def createAmatrix_Bindependent(self):
+        """
+        Creates lists and functions used for the Amatrix calculations that are independent of the magnetic field
+        Then populates an Amatrix with terms independent of the magnetic field
+        """
         #n is the number of total points in our list, which is also the number of states in our Hilbert space,
         #and hence n x n is the size of our A matrix
-        self.n = self.initialPointsInstance.n_points*self.initialPointsInstance.n_cuts
+        self.n = self.FSorbitsInstance.n_points*self.FSorbitsInstance.n_cuts
 
         #create A matrix to populate with numbers:
-        self.A = np.zeros((self.n,self.n))
+        self.A_Bindependent = np.zeros((self.n,self.n))
 
         #Amatrixpositionlist[i,j] gives the row (or column) in self.A that corresponds to state self.orbitsInstance.orbitsEQS[i][j]
-        Amatrixpositionlist = np.arange(0, self.initialPointsInstance.n_points*self.initialPointsInstance.n_cuts).reshape(self.initialPointsInstance.n_points, self.initialPointsInstance.n_cuts)
+        self.Amatrixpositionlist = np.arange(0, self.FSorbitsInstance.n_points*self.FSorbitsInstance.n_cuts).reshape(self.FSorbitsInstance.n_points, self.FSorbitsInstance.n_cuts)
 
         #find state[i+1] - state[i-1] for states as you traverse an "in-plane" orbit, then compute their norms and unit vectors used to compute gradients
         deltaplist_inplane = []
-        for orbit in self.orbitsInstance.orbitsEQS:
+        for orbit in self.FSorbitsInstance.FSorbits:
             orbit_plus1 = np.roll(orbit,-1,axis=0)
             orbit_minus1 = np.roll(orbit,1,axis=0)
             deltaplist_inplane.append(orbit_plus1 - orbit_minus1)
         deltaparray_inplane =   np.array([value for sublist in deltaplist_inplane for value in sublist]) #this can be simplified, fix later
-        deltaparray_inplane_norms = np.linalg.norm(deltaparray_inplane,axis=1)
-        deltaparray_inplane_unitvectors = deltaparray_inplane/deltaparray_inplane_norms[:,None]
+        self.deltaparray_inplane_norms = np.linalg.norm(deltaparray_inplane,axis=1)
+        self.deltaparray_inplane_unitvectors = deltaparray_inplane/self.deltaparray_inplane_norms[:,None]
 
         #find state[i+1] - state[i-1] for states as you traverse an "out of plane" orbit, then compute their norms and unit vectors used to compute gradients
         deltaplist_outofplane = []
-        for id,orbit in enumerate(self.orbitsInstance.orbitsEQS):
-            orbit_plus1 = self.orbitsInstance.orbitsEQS[(id+1)%len(self.orbitsInstance.orbitsEQS)]
-            orbit_minus1 = self.orbitsInstance.orbitsEQS[(id-1)%len(self.orbitsInstance.orbitsEQS)]
-            BZlength = (2*np.pi)/self.initialPointsInstance.c
+        for id,orbit in enumerate(self.FSorbitsInstance.FSorbits):
+            orbit_plus1 = self.FSorbitsInstance.FSorbits[(id+1)%len(self.FSorbitsInstance.FSorbits)]
+            orbit_minus1 = self.FSorbitsInstance.FSorbits[(id-1)%len(self.FSorbitsInstance.FSorbits)]
+            BZlength = (2*np.pi)/self.FSorbitsInstance.c
             deltaplist_outofplane.append((orbit_plus1 - orbit_minus1 + BZlength/2)%(BZlength) - (BZlength/2))
         deltaparray_outofplane =   np.array([value for sublist in deltaplist_outofplane for value in sublist]) #this can be simplified, fix later
-        deltaparray_outofplane_norms = np.linalg.norm(deltaparray_outofplane,axis=1)
-        deltaparray_outofplane_unitvectors = deltaparray_outofplane/deltaparray_outofplane_norms[:,None]
-
-        #number that denotes the index of the beginning of the current submatrix
-        submatrixindex = 0
-        submatrixindexlist = [0] #keeps track of all submatrix starting points
+        self.deltaparray_outofplane_norms = np.linalg.norm(deltaparray_outofplane,axis=1)
+        self.deltaparray_outofplane_unitvectors = deltaparray_outofplane/self.deltaparray_outofplane_norms[:,None]
 
         #creates a list of states in the same order as they would appear in the double loop
-        stateslist = np.array([state for orbit in self.orbitsInstance.orbitsEQS for state in orbit]) #stateslist[i] = ith state
-        invtaulist = self.dispersionInstance.invtau(np.transpose(stateslist)) #invtaulist[i]  = invtau(stateslist[i])
+        stateslist = np.array([state for orbit in self.FSorbitsInstance.FSorbits for state in orbit]) #stateslist[i] = ith state
+        self.invtaulist = self.dispersionInstance.invtau(np.transpose(stateslist)) #invtaulist[i]  = invtau(stateslist[i])
         self.dedk_list = np.transpose(self.dispersionInstance.dedk(np.transpose(stateslist))) #self.dedk_list[i] = dedk(stateslist[i])
+
+        #now populate the Amatrix with B independent terms
+        i=0 #i and j correspond to the ith orbit and jth state on that orbit that is being iterated
+        for orbit in self.FSorbitsInstance.FSorbits:
+            j=0
+            for state in orbit:
+                #diagonal term coming from scattering out
+                Amatrixposition = self.Amatrixpositionlist[i,j]
+                self.A_Bindependent[Amatrixposition,Amatrixposition] += self.invtaulist[Amatrixposition]
+                j+= 1
+            i+= 1
+
+    def createAmatrix_Bdependent(self,B):
+        self.A = np.copy(self.A_Bindependent) #create a copy of the B independent Amatrix to populate with B dependent terms
+        self.B = B
         crosslist  = np.cross(self.dedk_list,self.B) #crosslist[i]  = dedk(state[i]) x B
-        dotlist_inplane = np.sum(crosslist*deltaparray_inplane_unitvectors,axis=1) #dotlist_inplane[i] = (dedk(state[i]) x B) . unitvec(state[i+1] - state[i-1])
-        dotlist_outofplane = np.sum(crosslist*deltaparray_outofplane_unitvectors,axis=1) #dotlist_outofplane[i] = (dedk(state[i]) x B) . unitvec(state[i+1] - state[i-1]) for out of plane states
-        graddatalist_inplane = dotlist_inplane/(deltaparray_inplane_norms*(6.582119569**2)) #graddatalist[i] = (dedk(state[i]) x B) . unitvec(state[i+1] - state[i-1])/norm(state[i+1] - state[i-1])
-        graddatalist_outofplane = dotlist_outofplane/(deltaparray_outofplane_norms*(6.582119569**2)) #graddatalist[i] = (dedk(state[i]) x B) . unitvec(state[i+1] - state[i-1])/norm(state[i+1] - state[i-1]) for out of plane states
+        dotlist_inplane = np.sum(crosslist*self.deltaparray_inplane_unitvectors,axis=1) #dotlist_inplane[i] = (dedk(state[i]) x B) . unitvec(state[i+1] - state[i-1])
+        dotlist_outofplane = np.sum(crosslist*self.deltaparray_outofplane_unitvectors,axis=1) #dotlist_outofplane[i] = (dedk(state[i]) x B) . unitvec(state[i+1] - state[i-1]) for out of plane states
+        graddatalist_inplane = dotlist_inplane/(self.deltaparray_inplane_norms*(6.582119569**2)) #graddatalist[i] = (dedk(state[i]) x B) . unitvec(state[i+1] - state[i-1])/norm(state[i+1] - state[i-1])
+        graddatalist_outofplane = dotlist_outofplane/(self.deltaparray_outofplane_norms*(6.582119569**2)) #graddatalist[i] = (dedk(state[i]) x B) . unitvec(state[i+1] - state[i-1])/norm(state[i+1] - state[i-1]) for out of plane states
 
         i=0 #i and j correspond to the ith orbit and jth state on that orbit that is being iterated
-        n = len(self.orbitsInstance.orbitsEQS) #n is the number of orbits
-        for orbit in self.orbitsInstance.orbitsEQS:
+        n = len(self.FSorbitsInstance.FSorbits) #n is the number of orbits
+        for orbit in self.FSorbitsInstance.FSorbits:
             m = len(orbit) #m is the number of states on the current orbit
             j=0
 
-            for state_id,state in enumerate(orbit):
-                #diagonal term coming from scattering out
-                Amatrixposition = Amatrixpositionlist[i,j]
-                self.A[Amatrixposition,Amatrixposition] += invtaulist[Amatrixposition]
+            for state in orbit:
+                Amatrixposition = self.Amatrixpositionlist[i,j]
 
                 #off diagonal terms that simulate the derivative term from the boltzmann equation, in plane
-                next_Amatrixposition_inplane = Amatrixpositionlist[i,(j+1)%m]
-                prev_Amatrixposition_inplane = Amatrixpositionlist[i,(j-1)%m]
+                next_Amatrixposition_inplane = self.Amatrixpositionlist[i,(j+1)%m]
+                prev_Amatrixposition_inplane = self.Amatrixpositionlist[i,(j-1)%m]
  
                 graddata_inplane = graddatalist_inplane[Amatrixposition]
 
@@ -85,8 +96,8 @@ class Conductivity:
                 self.A[Amatrixposition,prev_Amatrixposition_inplane] += -graddata_inplane
 
                 #off diagonal terms that simulate the derivative term from the boltzmann equation, out of plane
-                next_Amatrixposition_outofplane = Amatrixpositionlist[(i+1)%n,j]
-                prev_Amatrixposition_outofplane = Amatrixpositionlist[(i-1)%n,j]
+                next_Amatrixposition_outofplane = self.Amatrixpositionlist[(i+1)%n,j]
+                prev_Amatrixposition_outofplane = self.Amatrixpositionlist[(i-1)%n,j]
 
                 graddata_outofplane = graddatalist_outofplane[Amatrixposition]
 
@@ -100,14 +111,12 @@ class Conductivity:
         #now add in scattering in terms coming from forward scattering
 
         #first create matrix whose {i,j} element is {k_i,k_j}, which will be an input to the scattering-in formula
-        plist = np.concatenate(self.orbitsInstance.orbitsEQS) #list of all momentum vectors in correct order
+        plist = np.concatenate(self.FSorbitsInstance.FSorbits) #list of all momentum vectors in correct order
 
         indices = np.indices([self.n,self.n]) #list of indices {i,j} to be extracted from plist  
 
         pi_minus_pj = (plist[indices])[0] - (plist[indices])[1] #the i,jth element of this matrix is p_i - p_j (directly features into the scattering in matrix)
          
-
-
 
     def createAlpha(self):
         #creates an array of the cartesian components of the velocity at each point on the discretized fermi surface
@@ -123,11 +132,11 @@ class Conductivity:
         self.sigma = np.zeros([3,3])
 
         #perptermlist[i] is a vector that lies along the fermi surface, pointing from the ith point to the orbit above it
-        perptermlist = self.dispersionInstance.dkperp(self.initialPointsInstance.dkz,self.initialPointsInstance.dkz,self.dedk_list)
+        perptermlist = self.dispersionInstance.dkperp(self.FSorbitsInstance.dkz,self.FSorbitsInstance.dkz,self.dedk_list)
 
         #nextstatepointerarray[i] is a vector that lies along the fermi surface and points from the ith point to the succeeding point on a given orbit
         nextstatepointerlist = []
-        for orbit in self.orbitsInstance.orbitsEQS:
+        for orbit in self.FSorbitsInstance.FSorbits:
             orbit_plus1 = np.roll(orbit,-1,axis=0)
             nextstatepointerlist.append(orbit - orbit_plus1)
         nextstatepointerarray =   np.array([value for sublist in nextstatepointerlist for value in sublist])
