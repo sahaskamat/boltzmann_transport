faa2cc40caab207ec5181ca5106882b913ce0027
sahaskamat <sahaskamat@yahoo.co.in>
2026-06-03 16:56:05 -0400

    added pipipi, pipizero_plus_fwd and pi_minus_delta scattering kernels

--- transport/scattering_kernels.py
+++ transport/scattering_kernels.py
@@ -38,23 +38,30 @@ def pipizero(deltak,g,strength,spread_xy,spread_z,n):
     qx,qy,qz = deltak[:,0],deltak[:,1],deltak[:,2]
     return (strength/((spread_xy**2)*spread_z))*np.exp(-(((np.abs(qx)-g[0]/2)**2 + (np.abs(qy)-g[1]/2)**2)/(spread_xy**2))**n)*np.exp(-(qz**2)/(spread_z**2))
 
-def pipizero_plus_fwd(deltak,g,strength,spread_xy,spread_z,n):
+def pipipi(deltak,g,strength,spread_xy,spread_z,n):
     qx,qy,qz = deltak[:,0],deltak[:,1],deltak[:,2]
-    return (strength/((spread_xy**2)*spread_z))*np.exp(-(((np.abs(qx)-g[0]/2)**2 + (np.abs(qy)-g[1]/2)**2)/(spread_xy**2))**n)*np.exp(-(qz**2)/(spread_z**2))
+    return (strength/((spread_xy**2)*spread_z))*np.exp(-(((np.abs(qx)-g[0]/2)**2 + (np.abs(qy)-g[1]/2)**2)/(spread_xy**2))**n)*np.exp(-((np.abs(qz)-g[2]/2)**2)/(spread_z**2))
 
-def piminusdelta_plus_fwd(deltak,g,strength_pipi,spread_z_pipi,strength_fwd):
+def pipizero_plus_fwd(deltak,g,strength,spread_xy,spread_z,n,strength_fwd,spread_z_fwd,spread_xy_fwd):
+    qx,qy,qz = deltak[:,0],deltak[:,1],deltak[:,2]
+    pipizero =  (strength/((spread_xy**2)*spread_z))*np.exp(-(((np.abs(qx)-g[0]/2)**2 + (np.abs(qy)-g[1]/2)**2)/(spread_xy**2))**n)*np.exp(-(qz**2)/(spread_z**2))
+    fwd = (strength_fwd/((spread_z_fwd)*(spread_xy_fwd**2)))*np.exp(-(qx**2 + qy**2)/(spread_xy_fwd**2))*np.exp(-(qz**2)/(spread_z_fwd**2))
+    return pipizero + fwd
+
+def piminusdeltapiminusdeltazero(deltak,g,strength,spread_xy,spread_z,delta,n):
     qx,qy,qz = deltak[:,0],deltak[:,1],deltak[:,2]
 
-    #scattering peaked at (pi-delta,pi-delta,0)
-    spread_xy = 0.04*g[0]
-    delta = 0.05*g[0]
-    piminusdelta = g[0]*0.5 - delta
-    pi = g[0]*0.5
-    n = 1
-    piminusdelta = (strength_pipi/((spread_xy**2)*spread_z_pipi))*(np.exp(-(((np.abs(qx)-piminusdelta)**2 + (np.abs(qy)-pi)**2)/(spread_xy**2))**n)+np.exp(-(((np.abs(qx)-pi)**2 + (np.abs(qy)-piminusdelta)**2)/(spread_xy**2))**n))*np.exp(-(qz**2)/(spread_z_pipi**2))
+    piminusdelta = (g[0]/2)*(1-delta)
+    piplusdelta = (g[0]/2)*(1+delta)
+    pi = (g[0]/2)
 
-    #forward scattering
-    spread_xy_fwd = 0.5*g[0]
-    forward = (strength_fwd/(spread_xy_fwd**2))*np.exp((qx**2 + qy**2)/(spread_xy_fwd**2))
+    pi_piminusdelta_gaussian = np.exp(-(((np.abs(qx)-piminusdelta)**2 + (np.abs(qy)-pi)**2)/(spread_xy**2))**n)
+    pi_piplusdelta_gaussian = np.exp(-(((np.abs(qx)-piplusdelta)**2 + (np.abs(qy)-pi)**2)/(spread_xy**2))**n)
+    piminusdelta_pi_gaussian = np.exp(-(((np.abs(qx)-pi)**2 + (np.abs(qy)-piminusdelta)**2)/(spread_xy**2))**n)
+    piplusdelta_pi_gaussian = np.exp(-(((np.abs(qx)-pi)**2 + (np.abs(qy)-piplusdelta)**2)/(spread_xy**2))**n)
 
-    return piminusdelta + forward
+    zaxis_gaussian = np.exp(-(qz**2)/(spread_z**2))
+
+    prefactor = (strength/((spread_xy**2)*spread_z))*0.25
+
+    return prefactor*(pi_piminusdelta_gaussian+pi_piplusdelta_gaussian+piminusdelta_pi_gaussian+piplusdelta_pi_gaussian)*zaxis_gaussian
