d96c4ec9b1041a50d447b1424487309481e3ee99
sahaskamat <sahaskamat@yahoo.co.in>
2026-07-13 10:24:51 -0400

    better fit params for 22%

--- fitting/makesinglecurve.py
+++ fitting/makesinglecurve.py
@@ -16,7 +16,7 @@ res_xy = 100
 #0.08092599477597432,11.26784244076325,16.610774375940203,0.11172046324944028,0.3944838562322481,1.4988975667416478
 
 #0.08803848932294113,0.41445800233548447,140.3929311204074,0.08340318765132346,0.2731068119348874,1.9755450186340013,2.531892490692818,0.2446595289952605,0.7457752153124259
-Tzmultvalue,invtau_iso,strength,spread_xy,n = 0.0325,9,50000000,0.143,6.3
+Tzmultvalue,invtau_iso,strength,spread_xy,n,strength_fwd,spread_fwd = 0.032,10,50000,0.143,6.3,0,0.2
 mumultvalue=0.805
 
 plotScattering=False
@@ -35,9 +35,16 @@ def create_rhozz(phi,Bmag,scattering_in=True,plotScattering=plotScattering):
     conductivityInstance = conductivity.Conductivity(dispersionInstance,FSorbitsInstance,invtau_iso=invtau_iso,delta_in_k=True)
     starttime = time()
     conductivityInstance.createAmatrix_Bindependent_isotropic()
+
     conductivityInstance.create_Hfunc(scatteringmodel="pipidelta",strength=strength,spread_xy=spread_xy,n=n)
     conductivityInstance.createAmatrix_Bindependent_fwdscatter_out()
     if scattering_in: conductivityInstance.createAmatrix_Bindependent_fwdscatter_in()
+
+    #conductivityInstance.delta_in_k = False
+    #conductivityInstance.create_Hfunc(scatteringmodel="pancake",strength=strength_fwd,spread_xy=spread_fwd,spread_z=spread_fwd)
+    #conductivityInstance.createAmatrix_Bindependent_fwdscatter_out()
+    #if scattering_in: conductivityInstance.createAmatrix_Bindependent_fwdscatter_in()
+
     if plotScattering: conductivityInstance.plotScatteringOut()
     #conductivityInstance.LUdecomp(B_for_LU)
     endtime = time()
